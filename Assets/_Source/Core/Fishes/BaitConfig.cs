5d8e2bc38f7dcfb9699bf3c21fa038d9fd1b6ecf
Alan Kasper <stsianastenka@tut.by>
2024-03-17 19:43:53 +0300

    fix: fishes scripts refactoring

---
+++ Assets/_Source/Core/Fishes/BaitConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core
+{
+  [Serializable]
+  public struct BaitConfig
+  {
+    public ushort BaitModelId;
+    [Range(1, 100)] public byte AttractionLevel;
+  }
+}
---
+++ Assets/_Source/Core/Fishes/FishBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Locations;
+using UnityEngine;
+
+namespace Core
+{
+  [CreateAssetMenu(fileName = "FishBehaviour")]
+  public class FishBehaviour : ScriptableObject
+  {
+    private FishGenus _genus;
+
+    private int _minWeight;
+    private int _maxWeight;
+
+    public AnimationCurve _distributionOfWeight = new(new Keyframe(0, 0), new Keyframe(1, 0));
+
+    public int[] DailyActivity = new int[24];
+    
+    [SerializeField]
+    private List<BaitConfig> _fishPreferences = new ();
+
+  }
+}
