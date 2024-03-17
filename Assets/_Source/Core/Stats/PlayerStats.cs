e48aeb9edf575e1a0cbf098122e5163ba7212adf
Anton Pauliuchyk <tauifyn@mail.ru>
2024-03-17 18:10:42 +0300

    fix: folder for finanse model

---
+++ Assets/_Source/Core/Stats/PlayerStats.cs
@@ -0,0 +1,10 @@
+public class PlayerStats 
+{
+    public string Name { get; set; }
+    public int Discharge { get; private set; }
+    public int TimeInGame { get; private set; }
+    public int CountVisitedLocations { get; private set; }
+    public int Karma { get; private set; }
+    public int Experience { get; set; }
+    public FishGenus FavouriteFish { get; set; } 
+}
