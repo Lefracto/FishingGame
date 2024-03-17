b1453ceb6a3f1d1ce38c11d8365863211f997505
Anton Pauliuchyk <tauifyn@mail.ru>
2024-03-17 17:58:15 +0300

    feat: add Editors/Inspectors and their little refactoring

---
+++ Assets/Editor/Inspectors/FishDataEditor.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+
+namespace Editor
+{
+  [CustomEditor(typeof(FishGenus))]
+  public class FishDataEditor : UnityEditor.Editor
+  {
+    private FishGenus _fish;
+    private const int FISH_IMAGE_HEIGHT = 167;
+    private const int FISH_IMAGE_WIDTH = 250;
+
+    private const string FISH_NAME_LABEL = "Fish Name";
+    private const string MIN_SCORED_WEIGHT_LABEL = "Min Scoring Weight";
+    private const string DESCRIPTION_LABEL = "Description of Fish";
+    private const string IMAGE_PREFIX = "Fish";
+    
+    public override void OnInspectorGUI()
+    {
+      _fish = (FishGenus)target;
+      serializedObject.Update();
+
+      _fish.Name = EditorGUILayout.TextField(FISH_NAME_LABEL, _fish.Name);
+      _fish.MinScoringWeight = EditorGUILayout.IntField(MIN_SCORED_WEIGHT_LABEL, _fish.MinScoringWeight);
+      _fish.FishImage = AssetVisualEditor.SpriteEditor(_fish.FishImage, FISH_IMAGE_HEIGHT, FISH_IMAGE_WIDTH, IMAGE_PREFIX);
+      EditorGUILayout.Space(AssetVisualEditor.POST_ICON_SPACE);
+      _fish.Description = EditorGUILayout.TextField(DESCRIPTION_LABEL, _fish.Description);
+      
+      serializedObject.ApplyModifiedProperties();
+    }
+  }
+}
---
+++ Assets/Editor/Inspectors/ModelsEditors/RodModelEditor.cs
@@ -0,0 +1,28 @@
+﻿using Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+  [CustomEditor(typeof(RodModel))]
+  public class RodModelEditor : UnityEditor.Editor
+  {
+    private RodModel _rod;
+    private const int ROD_IMAGE_HEIGHT = 167;
+    private const int ROD_IMAGE_WIDTH = 250;
+    
+    private const string MAX_WEIGHT_TEXT = "Max Weight (kg)";
+    
+    public override void OnInspectorGUI()
+    {
+      _rod = (RodModel)target;
+      AssetVisualEditor.TackleModelEditor(_rod, ROD_IMAGE_HEIGHT, ROD_IMAGE_WIDTH);
+      GUILayout.Space(AssetVisualEditor.POST_ICON_SPACE);
+      _rod.MaxWeight = EditorGUILayout.IntField(MAX_WEIGHT_TEXT, _rod.MaxWeight);
+      serializedObject.ApplyModifiedProperties();
+
+      if (GUI.changed)
+        EditorUtility.SetDirty(target);
+    }
+  }
+}
